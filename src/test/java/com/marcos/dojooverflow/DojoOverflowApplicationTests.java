9716914819508405ffb3eec303efa316c339dae5
Mrck0s <110942627+Mrck0s@users.noreply.github.com>
2023-08-14 12:02:25 -0300

    Initial commit

---
+++ src/test/java/com/marcos/dojooverflow/DojoOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.dojooverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoOverflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
