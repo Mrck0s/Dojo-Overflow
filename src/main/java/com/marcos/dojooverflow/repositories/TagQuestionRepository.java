691b1d2cf766b8cb329b5566a7f5790275821e52
Mrck0s <110942627+Mrck0s@users.noreply.github.com>
2023-08-23 12:06:32 -0300

    upd

--- src/main/java/com/marcos/dojooverflow/repositories/TagQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marcos.dojooverflow.repositories;
-
-import com.marcos.dojooverflow.models.TagQuestion;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagQuestionRepository extends CrudRepository<TagQuestion, Long> {
-}
